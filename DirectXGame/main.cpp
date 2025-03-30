880741f2f5f7ac136a7f2a58302540b4372309e9
iKedaTakumi07 <k024g0003@g.neec.ac.jp>
2025-03-30 23:49:10 +0900

    何にも書いていない状態に変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -33,42 +33,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	audio = Audio::GetInstance();
 	audio->Initialize();
 
-	// サウンドデータハンドル
-	uint32_t soundDataHandle_ = 0;
-	soundDataHandle_ = audio->LoadWave("fanfare.wav");
-
-	// 音声再生ハンドル
-	uint32_t voiceHandel_ = 0;
-
 	// テクスチャマネージャの初期化
 	TextureManager::GetInstance()->Initialize(dxCommon->GetDevice());
 	TextureManager::Load("white1x1.png");
 
-	// スプライト
-	/*uint32_t textureHandel_ = 0;
-	textureHandel_ = TextureManager::Load("sample.png");*/
-
-	// モデル
-	uint32_t textureHandel_2 = 0;
-	textureHandel_2 = TextureManager::Load("sample.png");
-
 	// スプライト静的初期化
 	Sprite::StaticInitialize(dxCommon->GetDevice(), WinApp::kWindowWidth, WinApp::kWindowHeight);
 
-	// スプライト
-	/*Sprite* sprite_ = nullptr;
-	sprite_ = Sprite::Create(textureHandel_, {100, 50});*/
-
 	// 3Dモデル静的初期化
 	Model::StaticInitialize();
-
-	// 3Dモデル
-	Model* model_ = nullptr;
-	model_ = Model::Create();
-
-	// ワールドトランスフォーム
-	WorldTransform worldTransform_;
-	worldTransform_.Initialize();
 
 	// カメラ
 	Camera camera;
@@ -90,15 +63,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			break;
 		}
 
-		// スプライト
-		// スプライトの今の座標を所得
-		// Vector2 position = sprite_->GetPosition();
-		//// 座標を{2,1}移動
-		// position.x += 2.0f;
-		// position.y += 1.0f;
-		//// 移動した座標をスプライトに反映
-		// sprite_->SetPosition(position);
-
 		// ImGui受付開始
 		imguiManager->Begin();
 		// 入力関連の毎フレーム処理
@@ -108,34 +72,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// ImGui受付終了
 		imguiManager->End();
 
-		// 音声再生
-		// audio->PlayWave(soundDataHandle_);
-
-		voiceHandel_ = audio->PlayWave(soundDataHandle_, true, 0.5f);
-
-		if (input->TriggerKey(DIK_SPACE)) {
-			// 音声停止
-			audio->StopWave(voiceHandel_);
-		}
-
 		// 描画開始
 		dxCommon->PreDraw();
-
-		// スプライト
-		//// スプライト描画前処理
-		// Sprite::PreDraw(dxCommon->GetCommandList());
-
-		//// スプライトを描画
-		// sprite_->Draw();
-
-		//// スプライト描画後処理
-		// Sprite::PostDraw();
-
-		// モデル
-		Model::PreDraw(dxCommon->GetCommandList());
-		model_->Draw(worldTransform_, camera, textureHandel_2);
-		Model::PostDraw();
-
 		// 軸表示の描画
 		axisIndicator->Draw();
 		// プリミティブ描画のリセット
@@ -145,9 +83,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// 描画終了
 		dxCommon->PostDraw();
 	}
-
-	/*delete sprite_;*/
-	delete model_;
 
 	// 3Dモデル解放
 	Model::StaticFinalize();
